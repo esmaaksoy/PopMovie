b1983511b559d6eec50929db182ce17290ee9af3
esma <esma.ea.aksoy@gmail.com>
2024-02-22 16:16:04 +0300

    feat: folder organization, and router configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import AppRouter from "./router/AppRouter";
+
 function App() {
-  return (
-    <div>
-      <h1>React App</h1>
-    </div>
-  )
+  return <AppRouter />;
 }
 
-export default App
+export default App;
