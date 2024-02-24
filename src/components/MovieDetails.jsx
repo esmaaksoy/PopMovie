64e479a09dbfbee7ff0e6fe1198d3161a92e53f1
esma <esma.ea.aksoy@gmail.com>
2024-02-24 14:27:04 +0300

    feat(MovieDetail): Created MovieDetail component in Detail Page

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,58 @@
+import avatar from "../assets/avatar.png";
+
+const MovieDetails = ({
+  actor,
+  production_companies,
+  tagline,
+  production_countries,
+}) => {
+  const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+  return (
+    <div className="detailSection">
+      <div className="detailContainer">
+        <div>
+          <p className="heading">Casts</p>
+          <div className="casts">
+            {actor.slice(0, 6).map((item, index) => (
+              <div className="castCard" key={index}>
+                <img
+                  alt="cast"
+                  src={item.profile_path ? IMG_API + item.profile_path : avatar}
+                />
+                <div className="castText">
+                  <p>{item.name}</p>
+                  <p>{item.character}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <p className="heading">Movie Details</p>
+          <div className="movieDetail">
+            <div className="companies">
+              <p>Companies</p>
+              <div className="companyNames">
+                {production_companies?.slice(0, 2).map((item, index) => (
+                  <p key={index}>{item?.name}</p>
+                ))}
+              </div>
+            </div>
+            <div className="companies">
+              <p>Countries</p>
+              <div className="companyNames">
+                {production_countries?.slice(0, 3).map((item, index) => (
+                  <p key={index}>{item?.name}</p>
+                ))}
+              </div>
+            </div>
+            <p className="tagline">{tagline}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetails;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -5,6 +5,7 @@ import ReactPlayer from "react-player";
 import defaultImage from "../assets/defaultImage.png";
 import { Helmet } from "react-helmet";
 import { LuPopcorn } from "react-icons/lu";
+import MovieDetails from "../components/MovieDetails";
 const Details = () => {
   const navigate = useNavigate();
   // Added code to extract the 'id' parameter from the URL using useParams.
@@ -13,21 +14,21 @@ const Details = () => {
   //Define state variables
   const [movieDetail, setMovieDetail] = useState({});
   const [video, setVideo] = useState({});
-
+  const [actor, setActor] = useState([]);
   // Define API key and URLs for movie details and video
   const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
   const movieDetailUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
   const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
-  // Function to fetch movie details
+  const movieActorUrl = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`;
+  // Function to fetch movie details,video data, cast data
   const getMovieDetail = async () => {
     try {
       const { data } = await axios(movieDetailUrl);
       setMovieDetail(data);
     } catch (error) {}
   };
-  // Function to fetch video data
+
   const getVideo = async () => {
     try {
       const { data } = await axios(videoUrl);
@@ -35,10 +36,18 @@ const Details = () => {
     } catch (error) {}
   };
 
+  const getActor = async () => {
+    try {
+      const { data } = await axios(movieActorUrl);
+      setActor(data.cast);
+    } catch (error) {}
+  };
+
   // Fetch movie details and video when the page loads
   useEffect(() => {
     getMovieDetail();
     getVideo();
+    getActor();
   }, []);
 
   // Destructure movie details
@@ -52,20 +61,22 @@ const Details = () => {
     vote_average,
     spoken_languages,
     runtime,
+    tagline,
+    production_countries,
   } = movieDetail;
 
   // Convert the total runtime in minutes to hours and minutes format.
   const hours = Math.floor(runtime / 60);
   const minutes = runtime % 60;
   const formattedRuntime = `${hours}h ${minutes}min`;
-  console.log(movieDetail)
+
   return (
     <>
       <Helmet>
         <title>Detail | Movie App</title>
         <meta name="description" content="Movie Detail Page" />
       </Helmet>
-      <div className="detail">
+      <div className="detailPage">
         <div className="homeIcon" onClick={() => navigate("/")}>
           <LuPopcorn className="icon" />
           <p>Home</p>
@@ -90,20 +101,20 @@ const Details = () => {
               <p className="date">({new Date(release_date).getFullYear()})</p>
             </div>
             <div className="genres">
-              {genres?.map((item) => (
-                <button>{item?.name}</button>
+              {genres?.map((item, index) => (
+                <button key={index}>{item?.name}</button>
               ))}
             </div>
             <div className="vote">
               <div>
-                {" "}
                 <p className="content">{vote_average?.toFixed(1)} / 10</p>
                 <p>Average</p>
               </div>
-
               <div>
-                {spoken_languages?.slice(0, 1).map((item) => (
-                  <p className="content">{item?.english_name}</p>
+                {spoken_languages?.slice(0, 1).map((item, index) => (
+                  <p className="content" key={index}>
+                    {item?.english_name}
+                  </p>
                 ))}
                 <p>Languages</p>
               </div>
@@ -116,17 +127,22 @@ const Details = () => {
               <p>Overview</p>
               <p>{overview}</p>
             </div>
-            <div className="companies">
+            {/* <div className="companies">
               <p>Production Companies</p>
               <div className="companyNames">
-                {production_companies?.slice(0, 2).map((item) => (
-                  <p>{item?.name}</p>
+                {production_companies?.slice(0, 2).map((item, index) => (
+                  <p key={index}>{item?.name}</p>
                 ))}
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
-      
+        <MovieDetails
+          actor={actor}
+          production_companies={production_companies}
+          tagline={tagline}
+          production_countries={production_countries}
+        />
       </div>
     </>
   );
